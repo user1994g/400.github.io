14561fdabcd11c4bc572b596803b480a8c8aa289
enidsinclair <jazzg869@gmail.com>
2026-07-31 03:46:24 +0100

    Update Final Lesson artwork

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 const rows = [
   { title: 'Films', sub: 'FEATURED TONIGHT', id: 'films', items: [
-    ["The dark echo’s of 1939",'noc-dark-echoes',1939,98,'15',null,true,null,true,'https://www.brooksbychurch.co.uk/wp-content/uploads/2014/08/church-tour-293x238.jpg'], ['The Final Lesson AP 1','noc-final-lesson-ap1',2026,96,'15',null,true,'https://clip-kingdom-play.lovable.app/embed/878b4496-ab7a-47fe-8e0f-0b489311241c',true,'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHo-l09FSg8XHSPuEZax5Vindkz8AmG3DJcg&s'], ['Hollow Coast','noc-hollowcoast',2026,97,'18'], ['Paper Moths','noc-papermoths',2024,94,'12'], ['Glass Orchard','noc-glassorchard',2023,92,'PG'], ['Salt & Bone','noc-saltbone',2024,97,'15'], ['The Last Broadcast','noc-last-broadcast',2025,95,'15',null,false,null,false], ['Rooms Without Doors','noc-rooms-doors',2024,91,'12',null,false,null,false], ['Ashes at Dawn','noc-ashes-dawn',2026,94,'18',null,false,null,false], ['The Blue Hour','noc-blue-hour',2025,89,'15',null,false,null,false]
+    ["The dark echo’s of 1939",'noc-dark-echoes',1939,98,'15',null,true,null,true,'https://www.brooksbychurch.co.uk/wp-content/uploads/2014/08/church-tour-293x238.jpg'], ['The Final Lesson AP 1','noc-final-lesson-ap1',2026,96,'15',null,true,'https://clip-kingdom-play.lovable.app/embed/878b4496-ab7a-47fe-8e0f-0b489311241c',true,'https://upload.wikimedia.org/wikipedia/commons/3/30/Brooksby_Hall_-_geograph.org.uk_-_584614.jpg'], ['Hollow Coast','noc-hollowcoast',2026,97,'18'], ['Paper Moths','noc-papermoths',2024,94,'12'], ['Glass Orchard','noc-glassorchard',2023,92,'PG'], ['Salt & Bone','noc-saltbone',2024,97,'15'], ['The Last Broadcast','noc-last-broadcast',2025,95,'15',null,false,null,false], ['Rooms Without Doors','noc-rooms-doors',2024,91,'12',null,false,null,false], ['Ashes at Dawn','noc-ashes-dawn',2026,94,'18',null,false,null,false], ['The Blue Hour','noc-blue-hour',2025,89,'15',null,false,null,false]
   ]},
   { title: 'Trending Now', sub: 'TOP 10 THIS WEEK', id: 'series', items: [
     ['Hollow Coast','noc-hollowcoast',2026,97,'18'], ['Nightjar','noc-nightjar',2025,90,'15'], ['Paper Moths','noc-papermoths',2024,94,'12'], ['The Understudy','noc-understudy',2026,89,'15'], ['Glass Orchard','noc-glassorchard',2023,92,'PG'], ["The Cartographer's Daughter",'noc-cartographer',2025,87,'12'], ['Ember & Rust','noc-emberrust',2024,95,'18']
@@ -52,7 +52,7 @@ const featuredTitles = [
     rating: '15',
     format: 'Feature film',
     description: 'One last class reveals a lesson no one was meant to learn. The Final Lesson AP 1 is available to watch now.',
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHo-l09FSg8XHSPuEZax5Vindkz8AmG3DJcg&s',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/3/30/Brooksby_Hall_-_geograph.org.uk_-_584614.jpg',
     videoUrl: 'https://clip-kingdom-play.lovable.app/embed/878b4496-ab7a-47fe-8e0f-0b489311241c'
   }
 ];
